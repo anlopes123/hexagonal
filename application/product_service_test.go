2bae406ac97b584f1663a8192bfda5960ed781b0
Anizair Lopes <anlopes123@gmail.com>
2023-02-14 22:09:44 -0300

    Finalizado os teste de ProductService

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -24,3 +24,57 @@ func TestProductService_Get(t *testing.T) {
 	require.Equal(t, product, result)
 
 }
+
+func TestProductService_Create(t *testing.T) {
+	ctrl:= gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product:= mock_application.NewMockProductInterface(ctrl)
+	persistense:= mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistense.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service:= application.ProductService {
+		Persistense: persistense,
+	}
+	result, err:= service.Create("Product 1", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+}
+
+func TestProductService_Enable(t *testing.T) {
+	ctrl:= gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product:= mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil)
+	persistense:= mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistense.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service:= application.ProductService {
+		Persistense: persistense,
+	}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+	
+func TestProductService_Disable(t *testing.T) {
+	ctrl:= gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product:= mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(nil)
+	persistense:= mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistense.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service:= application.ProductService {
+		Persistense: persistense,
+	}
+
+	result, err := service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+	
